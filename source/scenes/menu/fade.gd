b6c5ab2a79cea0d4739102557e6ab9a56307774f
Gabriel Henrique <bielcampos.09@gmail.com>
2026-02-22 17:48:02 -0300

    Menu
    Funcionalidades de Menu principal, transição entra menus, configuração de tela cheia e inicio do jogo

---
+++ source/scenes/menu/fade.gd
@@ -0,0 +1,34 @@
+extends ProgressBar
+var playing = false
+var fadein = false
+var rate = 1.3
+
+var wait = 0
+var maxWait = 1
+
+signal blackout()
+
+func _process(delta: float) -> void:
+	if wait>0: wait -= delta
+	
+	if (playing && fadein): 
+		self.value = min(100, (value+delta)*rate)
+		if (self.value >= 100): 
+			fadein = false
+			fill_mode = FILL_BEGIN_TO_END
+			blackout.emit()
+	elif (playing && !fadein && wait <= 0): 
+		self.value = max(0, (value-delta)/rate)
+		if (self.value <= 0):
+			set_mouse_filter(MOUSE_FILTER_IGNORE)
+			playing = false
+			fill_mode = FILL_END_TO_BEGIN
+
+
+func play():
+	if (!playing):
+		playing = true
+		fadein = true
+		wait = maxWait
+		fill_mode = FILL_END_TO_BEGIN
+		set_mouse_filter(MOUSE_FILTER_STOP)
---
+++ source/scenes/menu/menu.gd
@@ -0,0 +1,63 @@
+extends Control
+var isInGame = false
+
+@onready var fade = $Fade
+
+@onready var titleMenu = $"Menu Inicial"
+@onready var configMenu = $"Configurações"
+@onready var achieveMenu = $"Conquistas"
+@onready var creditsMenu = $"Créditos"
+
+
+@onready var activemenu = titleMenu
+@onready var to = titleMenu
+
+func _ready() -> void:
+	ChangeMenu()
+
+##Menu Inicial
+func _on_jogar_pressed() -> void:
+	activemenu.hide()
+	
+
+func _on_configurações_pressed() -> void:
+	to = configMenu
+	FadePlay()
+
+func _on_conquistas_pressed() -> void:
+	to = achieveMenu
+	FadePlay()
+
+func _on_créditos_pressed() -> void:
+	to = creditsMenu
+	FadePlay()
+
+func _on_sair_pressed() -> void:
+	pass # Replace with function body.
+
+##Configurações
+func _on_tela_cheia_toggled(toggled_on: bool) -> void:
+	if (toggled_on):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) 
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) 
+	pass # Replace with function body.
+
+func _on_voltar_pressed() -> void:
+	to = titleMenu
+	FadePlay()
+
+
+func FadePlay():
+	if !fade.playing:
+		fade.play()
+	
+func ChangeMenu():
+	activemenu.hide()
+	activemenu = to
+	activemenu.show()
+	
+
+
+func _on_fade_blackout() -> void:
+	ChangeMenu()
